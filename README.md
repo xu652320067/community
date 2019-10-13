e08f04bd4096bd130ce51cbe3188bd51a8c5ac15
xupeng <xupeng@study.com>
2019-10-13 17:46:11 +0800

    保持登录状态cokie和session

--- README.md
+++ README.md
@@ -8,7 +8,13 @@ https://spring.io/guides/gs/serving-web-content/
 
 
 
-### 将配置文件分离,写到properties中去
+### 5将配置文件分离,写到properties中去
 在配置文件中定义Client_id,Client_secret,Redirect_uri
 通过注解的形式注入AutorizeController.java中
 
+### 6保持登录状态cokie和session
+session相当于银行账户,cokie就相当于银行卡,只有拿到银行卡才能知道账号是谁的,才能操作余额<br>
+浏览器就相当于"人",服务器相当于"银行",本身http是无状态的,如何让服务器记录状态,需要每次携带一张银行卡(cokie)来.<br>
+写一个cokie和session来表示登录状态,没有登录的时候只显示"登录",登录成功后只显示"我"<br>
+
+### 7数据库
--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -31,7 +33,9 @@ public class AuthorizeController {
      */
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) {
+                           @RequestParam(name="state") String state,
+                           //http是在HttpServletRequest拿到的,写此方法的时候,spring会自动将上下文中的request放到这个里面来
+                           HttpServletRequest request) {
         AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
         accesstokenDTO.setClient_id(clientId);
         accesstokenDTO.setClient_secret(clientSecret);
@@ -40,7 +44,15 @@ public class AuthorizeController {
         accesstokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accesstokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        //System.out.println(user.getName());
+        // 6判断user不为空
+        if(user!=null){
+            //登录成功,写cokie和session,在callback方法中添加HttpServletRequest
+            request.getSession().setAttribute("user",user); //将user对象放到session中,就相当于已经创建成功了一张银行卡,但是还并没有给前端银行卡,如果不手动给签前端,前端也会自动创建,但是不能选号
+            return "redirect:/"; //使用redirect重定向到index,下一步写index.html
+        }else {
+            //登录失败,重新登录
+            return "redirect:/"; //失败也跳转回index页面
+        }
     }
 }
